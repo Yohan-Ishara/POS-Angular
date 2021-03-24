93491527b3f44c7274e51bbf8c9958927300fc9b
Yohan Ishara <yohanishara01@gmail.com>
2021-03-24 13:42:01 +0530

    create customer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerPageComponent } from './components/customer-page/customer-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Customer',component:CustomerPageComponent},
+  {path:'',component:CustomerPageComponent},
+  {path:'**',component:NotFoundPageComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
